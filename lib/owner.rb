541b92f20cb2ed24d1fb2d6d3ac5ed71ab951034
Dominic Chapman <dominicchapman@me.com>
2017-10-11 21:52:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,9 +28,12 @@ class Owner
     "I am a #{self.species}."
   end
 
-
   def buy_fish(name)
     pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
   end
 
 end
@@ -39,9 +42,14 @@ end
 # Owner Class methods keeps track of the owners that have been created
 # Owner Class methods can reset the owners that have been created
 # Owner Class methods can count how many owners have been created
+
 # Owner instance methods #species initializes with a species
 # Owner instance methods #species can't change its species
+
 # Owner instance methods #say_species  can say its species
+
 # Owner instance methods #name can have a name
 # Owner instance methods #pets is initialized with a pets attribute as a hash with 3 keys
 # Owner instance methods #buy_fish can buy a fish that is an instance of the Fish class
+# Owner instance methods #buy_cat can buy a cat that is an instance of the Cat class
+# Owner instance methods #buy_cat knows about its cats
