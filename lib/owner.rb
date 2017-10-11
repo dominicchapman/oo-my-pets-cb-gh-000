b81c94478cd0ba6d178a69a76eecac6bac06d178
Dominic Chapman <dominicchapman@me.com>
2017-10-11 22:01:11 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,10 +60,10 @@ class Owner
 
   def sell_pets
     pets.each do |pet_type|
-      pet_type.clear
       pet_type.each do |pet|
         pet.mood = "nervous"
       end
+      pet_type.clear
     end
   end
 
