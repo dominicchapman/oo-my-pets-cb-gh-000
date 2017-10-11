3ec1574b71184261376bef1841b209b022514591
Dominic Chapman <dominicchapman@me.com>
2017-10-11 21:53:31 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -36,11 +36,9 @@ class Owner
     pets[:cats] << Cat.new(name)
   end
 
-  # def buy_dog(name)
-  #   pets[:dogs] << Dog.new(name)
-  # end
-
-
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
 
 end
 
