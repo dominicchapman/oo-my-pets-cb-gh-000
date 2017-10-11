fb0bb131ca606333b7f5f554aa1bf9c47a95c997
Dominic Chapman <dominicchapman@me.com>
2017-10-11 21:04:19 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
   # code goes here
+
+  @@owners = []
+
+  attr_reader :species
+
+  def initialize(species)
+    @species = species
+    @@owners << self
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+  def self.count
+    @@owners.count
+  end
+
+  def say_species
+    self.species
+  end
+
 end
+
+
+# Owner Class methods keeps track of the owners that have been created
+# Owner Class methods can reset the owners that have been created
+# Owner Class methods can count how many owners have been created
+# Owner instance methods #species initializes with a species
+# Owner instance methods #species can't change its species
